26dcf78e4f29f591e05346415407cb05f5b7823a
Patric Augusto <159172458+PatricAugusto@users.noreply.github.com>
2024-05-17 17:59:15 -0300

    Add files via upload

---
+++ Verificador de idade/script.js
@@ -0,0 +1,27 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById("txtano")
+    var res = document.getElementById("res")
+
+    if(fAno.value.length == 0 || Number(fAno.value) > ano){
+        alert("[ERRO] Verfique os dados e tente novamente")
+    } else{
+        var fsex = document.getElementsByName("radsex")
+        var idade = ano - Number(fAno.value)
+        var genero = ""
+        var img = document.createElement("img")
+
+        img.setAttribute("id", "foto")
+
+        if(fsex[0].checked){
+            genero = "Homem"
+
+        } else if (fsex[1].checked) {
+        genero = "Mulher"
+    }
+    res.style.textAlign = "center"
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+
+}
+}
